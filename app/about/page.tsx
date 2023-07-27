01925482323f23c4fe15f681db14808b5a446f85
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-07-27 15:26:38 +0900

    ♻️ ロゴ部分をコンポーネント化

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
+import { Logo } from "@/copornents/Logo "
 import { Linkarea } from "../../copornents/Linkarea"
 
 export default function Page (){
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>about</h1>
+      
+      <Logo page="about" />
       <Linkarea />
     </main>
     
