b1412601b2bce5d7c310ab0e9857efc1b70d930b
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-07-27 14:23:47 +0900

    ✨ aboutページの追加

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import { Linkarea } from "../../copornents/Linkarea"
+
+export default function Page (){
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>about</h1>
+      <Linkarea />
+    </main>
+    
+  )
+}
