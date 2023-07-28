be6048f2c9eceacc45b029119a3e86afb258ed42
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-07-28 15:13:13 +0900

    ♻️ ボタンクリックイベントのリファクタリング

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
+import { useCallback } from "react"
 import { Main } from "../copornents/Main"
 
 export default function Page (){
+  const handleClick = useCallback(() => {
+    alert("クリック");
+  }, []);
   return (
     <Main page="about"
-          onClick ={() => alert("クリック")} />
+          onClick ={handleClick} />
     
   )
 }
