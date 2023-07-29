b8afed0a54a639e2ef811e8b728dddb466ed7cf2
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-07-29 10:02:37 +0900

    ✨ useEffectの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+"use client"
+
+import { useEffect } from "react"
 import '@/src/app/globals.css'
 import { Main } from "@/src/app/copornents/Main";
 
 export default function Home() {
-  return (
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "red";
+
+    return () => {
+      document.body.style.backgroundColor = "yellow";
+    };
+  }, []);
+    return (
     <Main page="app"/>
   )
 }
