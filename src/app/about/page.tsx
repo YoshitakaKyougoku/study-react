cb3e496bee1f5387ae8220952c4aa0a1a0844757
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-07-30 16:03:18 +0900

    ✨ buttonの上限１０までに変更

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,16 +4,17 @@ import { useCallback, useState } from "react"
 import { Main } from "../copornents/Main"
 
 export default function Page (){
-  const [foo, setfoo] =  useState(1)
-  // let foo = 1;
-  const handleClick = () => {
-    setfoo((foo) => foo + 1);
-    // foo = foo + 1;
-  };
+  const [count, setCount] =  useState(1)
+  
+  const handleClick = useCallback(() => {
+    if (count < 10){
+      setCount((count) => count + 1);
+    }
+  },[count]);
   return (
     <>
       <div className="text-3xl font-serif font-bold  text-center">
-        <span className="transition duration-700 ease-in-out delay-150 hover:-translate-y-1 hover:scale-95">count  </span>{foo}
+        <span className="transition duration-700 ease-in-out delay-150 hover:-translate-y-1 hover:scale-95">count  </span>{count}
       </div>
       <Main page="about"
           onClick ={handleClick} />
