134eeb842b7afc0b8d449d3a68733f2483e0daf0
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-07-30 15:42:18 +0900

    ✨ ボタンのカウントアップ機能の追加

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,24 @@
 "use client"
 
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 import { Main } from "../copornents/Main"
 
 export default function Page (){
-  const handleClick = useCallback(() => {
-    alert("クリック");
-  }, []);
+  const [foo, setfoo] =  useState(1)
+  // let foo = 1;
+  const handleClick = () => {
+    setfoo((foo) => foo + 1);
+    // foo = foo + 1;
+  };
   return (
-    <Main page="about"
+    <>
+      <div className="text-3xl font-serif font-bold  text-center">
+        <span className="transition duration-700 ease-in-out delay-150 hover:-translate-y-1 hover:scale-95">count  </span>{foo}
+      </div>
+      <Main page="about"
           onClick ={handleClick} />
+    </>
+    
     
   )
 }
