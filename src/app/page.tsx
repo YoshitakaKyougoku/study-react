761e44c1f38d776ef9e1973549d6b88653389568
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-07-30 17:39:14 +0900

    ✨appにテキストエリアを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,32 @@
 "use client"
 
-import { useEffect } from "react"
+import { useCallback, useEffect, useState } from "react"
 import '@/src/app/globals.css'
 import { Main } from "@/src/app/copornents/Main";
 
 export default function Home() {
-
+  const [text, setText] = useState("");
+  
+  const handleChange = useCallback((e: any) => {
+    if (e.target.value.length > 5){
+      alert("5文字以内にして下さい");
+      return;
+    }
+    setText(e.target.value.trim());
+  },[]);
   useEffect(() => {
-    document.body.style.backgroundColor = "red";
+    document.body.style.backgroundColor = "";
 
     return () => {
-      document.body.style.backgroundColor = "yellow";
+      document.body.style.backgroundColor = "cyan";
     };
   }, []);
+  
     return (
-    <Main page="app"/>
+      <>
+        <input className="bg-slate-200 " type ="text" value={text} onChange={handleChange} />
+        <Main page="app"/>
+      </>
+      
   )
 }
