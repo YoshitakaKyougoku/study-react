23604ea1021d77ccddb61e81c181977f6752834d
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-07-30 17:40:44 +0900

    ✨ ボタンの改善

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,17 +5,31 @@ import { Main } from "../copornents/Main"
 
 export default function Page (){
   const [count, setCount] =  useState(1)
+  const [isShow, setIsShow] = useState(true);
+
   
   const handleClick = useCallback(() => {
-    if (count < 10){
+    if (count < 9){
       setCount((count) => count + 1);
+    } else {
+      setCount((count) => count * 0)
     }
   },[count]);
+
+  const handleDisplay = useCallback(() => {
+
+    setIsShow((isShow) => !isShow);
+
+  },[]);
   return (
     <>
-      <div className="text-3xl font-serif font-bold  text-center">
-        <span className="transition duration-700 ease-in-out delay-150 hover:-translate-y-1 hover:scale-95">count  </span>{count}
-      </div>
+      {isShow ? <div className="flex justify-center space-x-2 absolute inset-x-1/2 text-3xl font-serif font-bold ">
+        <p className="transition duration-700 ease-in-out delay-150 hover:-translate-x-1 hover:scale-95">{10 - count}<span className="text-white">+</span>
+        {count}<span className="text-white">=</span>10</p>
+      </div> :null}
+      <button className="transition ease-in-out delay-150 hover:translate-x-1 flex font-bold font-serif w-max content-center text-2xl p-3"
+              onClick={handleDisplay}>{isShow ? "hidden" :"display"}
+      </button>
       <Main page="about"
           onClick ={handleClick} />
     </>
