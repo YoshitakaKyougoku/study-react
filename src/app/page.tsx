10295513886273c921593d06e74fc481354f0d58
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-07-31 11:27:44 +0900

    ✨ useStateで配列を扱う

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,11 @@ import '@/src/app/globals.css'
 import { Main } from "@/src/app/copornents/Main";
 
 export default function Home() {
+  const [array, setArray] = useState<string[]>([]);
+
   const [text, setText] = useState("");
   
+  // テキストフォーム
   const handleChange = useCallback((e: any) => {
     if (e.target.value.length > 5){
       alert("5文字以内にして下さい");
@@ -14,6 +17,17 @@ export default function Home() {
     }
     setText(e.target.value.trim());
   },[]);
+
+  // 追加
+  const handleAdd = useCallback(() => {
+    setArray((prevArray: string[]) => {
+      if (prevArray.some(item => item === text)) {
+        alert("same text");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    })
+  },[text])
   useEffect(() => {
     document.body.style.backgroundColor = "";
 
@@ -21,10 +35,18 @@ export default function Home() {
       document.body.style.backgroundColor = "cyan";
     };
   }, []);
-  
+
     return (
       <>
         <input className="bg-slate-200 " type ="text" value={text} onChange={handleChange} />
+        <button onClick={handleAdd}>add</button>
+        <ul>
+          {array.map(item => {
+            return (
+              <li key={item}>{item}</li>
+            )
+          })}
+        </ul>
         <Main page="app"/>
       </>
       
